646cae4ffd0832a7cac535004ff343fe62dd05b7
Mark Plitt <markplitt@gmail.com>
2020-11-13 19:14:02 -0800

    catch up for thorlabs rig

---
+++ Assets/Scripts/ShortTrack/PC_ShortTrack.cs
@@ -0,0 +1,285 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+using System.Text;
+using System.Net;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+public class PC_ShortTrack : MonoBehaviour
+{
+
+    private static int localPort;
+
+    // prefs 
+
+    private static string IP = "171.65.17.36";  // define in init
+    private static int port = 7000;  // define in init
+
+    // "connection" things
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+
+    private GameObject player;
+    private GameObject panoCam;
+    private GameObject reward0;
+    private GameObject reward1;
+    private GameObject reward;
+
+
+
+
+    private Rigidbody rb;
+    private SP_ShortTrack sp;
+    private DL_ShortTrack dl;
+    private RR_ShortTrack rotary;
+
+    
+    private Vector3 initialPosition;
+    private Vector3 movement;
+    private static bool created = false;
+    private int r;
+    public int cmd = 2;
+
+    public int mRewardFlag = 0;
+    public int rzoneFlag = 0;
+    
+    public int tendFlag = 0;
+    public int tstartFlag = 0;
+
+
+
+    private void sendString(string message)
+    {
+        try
+        {
+            if (message != "")
+            {
+
+                // get UTF8 encoding of string
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                // send message
+                client.Send(data, data.Length, remoteEndPoint);
+            }
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+    
+
+    public void Awake()
+    {
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+        client = new UdpClient();
+
+        GameObject player = GameObject.Find("Player");
+        sp = player.GetComponent<SP_ShortTrack>();
+        rotary = player.GetComponent<RR_ShortTrack>();
+        dl = player.GetComponent<DL_ShortTrack>();
+        Debug.Log(sp.sceneName);
+        
+
+        reward = GameObject.Find("Reward");
+       
+        panoCam = GameObject.Find("panoCamera");
+        panoCam.transform.eulerAngles = new Vector3(0.0f, -90.0f, 0.0f);
+        initialPosition = new Vector3(0f, .5f, -50.0f);
+
+
+       
+    }
+
+    void Start()
+    {
+        StartCoroutine(FlagCheck());
+
+    }
+
+    void Update()
+    {
+
+        // make sure rotation angle is 0
+        transform.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+
+        // end game after appropriate number of trials
+        if ((sp.numTraversals >= sp.numTrialsTotal) | (sp.numRewards >= sp.maxRewards & transform.position.z < 0f))
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        }
+
+        if (dl.r > 0 & dl.rflag < 1) { StartCoroutine(DeliverReward(dl.r)); dl.rflag = 1; }; // deliver appropriate reward
+
+        // manual rewards and punishments
+        mRewardFlag = 0;
+        if (Input.GetKeyDown(KeyCode.Q) | Input.GetMouseButtonDown(0)) // reward left - sweetened condensed milk
+        {
+            mRewardFlag = 1;
+            StartCoroutine(DeliverReward(4));
+            sp.numRewards += 1;
+
+        }
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+
+
+        if (other.tag == "Reward")
+        {
+            StartCoroutine(RewardSequence(transform.position.z));
+
+        }
+        else if (other.tag == "Teleport")
+        {
+         
+
+            sp.numTraversals += 1;
+            tendFlag = 1;
+            transform.position = initialPosition;
+            
+
+            StartCoroutine(InterTrialTimeout());
+
+         
+        }
+        
+        
+       
+
+    }
+
+
+    IEnumerator FlagCheck()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            tendFlag = 0;
+            tstartFlag = 0;
+        }
+        yield return null;
+
+    }
+    IEnumerator InterTrialTimeout()
+    {
+
+        rotary.toutBool = 0f;
+        if (UnityEngine.Random.value > sp.skipTrialPcnt)
+        {
+            reward.SetActive(true);
+        }
+        else
+        {
+            reward.SetActive(false);
+        }
+
+        sendString("L0");
+        yield return new WaitForSeconds(5f + UnityEngine.Random.value * 2f);
+        sendString("L1");
+        yield return new WaitForSeconds(1f);
+       
+
+        rotary.toutBool = 1f;
+        
+        yield return null;
+    }
+
+    void OnApplicationQuit()
+    {
+        panoCam.SetActive(false);
+    }
+
+    IEnumerator RewardSequence(float pos)
+    {   // water reward
+        rzoneFlag = 1;
+
+        
+        while ((transform.position.z <= pos + 50))
+        {
+
+
+            if ((sp.AutoReward) & (transform.position.z > pos + 30)) 
+            {
+
+                
+                cmd = 4;
+                
+                StartCoroutine(DeliverReward(1));
+                sp.numRewards += 1;
+                yield return new WaitForEndOfFrame();
+                break;
+
+             
+            }
+
+            if ((dl.c_1 > 0))
+            {
+                
+                cmd = 4;
+                sp.numRewards += 1;
+                
+                yield return new WaitForEndOfFrame();
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+            
+        }
+        
+        
+
+
+
+
+        
+        rzoneFlag = 0;
+        reward.SetActive(false);
+        yield return new WaitForEndOfFrame();
+        
+        cmd = 2;
+
+    }
+
+    
+
+
+    IEnumerator DeliverReward(int r)
+    { // deliver
+
+        if (r == 1) // reward left
+        {
+            
+            //sp.numRewards += 1;
+            yield return null;
+        }
+        else if (r == 4)
+        {
+            cmd = 4;
+            //sp.numRewards += 1;
+            yield return new WaitForSeconds(0.01f);
+            cmd = 0;
+        }
+        else
+        {
+            yield return null;
+        };
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/ShortTrack/SP_ShortTrack.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+
+public class SP_ShortTrack : MonoBehaviour
+{
+
+    //public bool saveData = true;
+    private string mouse;
+
+    public float skipTrialPcnt = 0.0f;
+    public bool ClickOn = false;
+    public bool AutoReward = false;
+    public bool BlockWalls = false;
+
+    public int numRewards = 0;
+    public int numRewards_manual = 0;
+    public int rewardFlag = 0;
+
+    public int numTraversals = 0;
+    public int numTrialsTotal;
+    public int maxRewards = 200;
+
+
+    
+    public float rDur = 2;
+
+    // for saving data
+    public string localDirectory_pre = "C:/Users/markp/VR_Data/TwoTower/";
+    public string serverDirectory_pre = "G:\\My Drive\\VR_Data\\TwoTower";
+    public string localDirectory;
+    public string serverDirectory;
+    public string localPrefix;
+    public string serverPrefix;
+    public string sceneName;
+
+    public int session = 1;
+
+
+    private DateTime today;
+    private GameObject player;
+    private RR_ShortTrack rr;
+    private DL_ShortTrack dl;
+    private PC_ShortTrack pc;
+    private SbxTTLs_ShortTrack ttls;
+    private Notes notes;
+
+    private bool orderBool;
+
+    private IDbConnection _connection;
+    private IDbCommand _command;
+
+
+
+    public int scanning = 0;
+
+    public void Awake()
+    {
+
+        player = GameObject.Find("Player");
+        sceneName = SceneManager.GetActiveScene().name;
+
+        
+        rr = player.GetComponent<RR_ShortTrack>();
+        dl = player.GetComponent<DL_ShortTrack>();
+        pc = player.GetComponent<PC_ShortTrack>();
+        ttls = player.GetComponent<SbxTTLs_ShortTrack>();
+        notes = player.GetComponent<Notes>();
+        mouse = notes.mouse;
+
+
+        today = DateTime.Today;
+        Debug.Log(today.ToString("dd_MM_yyyy"));
+
+
+
+        localDirectory = localDirectory_pre + mouse + '/' + today.ToString("dd_MM_yyy") + '/';
+        serverDirectory = serverDirectory_pre + mouse + '/' + today.ToString("dd_MM_yyy") + '/';
+        if (!Directory.Exists(localDirectory))
+        {
+            Directory.CreateDirectory(localDirectory);
+        }
+        if (!Directory.Exists(serverDirectory))
+        {
+            Directory.CreateDirectory(serverDirectory);
+        }
+
+
+
+
+
+        bool nameFlag = true;
+        while (nameFlag)
+        {
+            localPrefix = localDirectory + "/" + sceneName + "_" + session.ToString();
+            serverPrefix = serverDirectory + "/" + sceneName + "_" + session.ToString();
+            if (File.Exists(localPrefix + ".sqlite"))
+            {
+                session++;
+            }
+            else
+            {
+                nameFlag = false;
+                SqliteConnection.CreateFile(localPrefix + ".sqlite");
+            }
+        }
+
+        string connectionString = "Data Source=" + localPrefix + ".sqlite;Version=3;";
+        _connection = (IDbConnection)new SqliteConnection(connectionString);
+        _connection.Open();
+        _command = _connection.CreateCommand();
+        _command.CommandText = "create table data (time REAL,  trialnum INT, pos REAL, dz REAL, lick INT, reward INT," +
+        "tstart INT, teleport INT, rzone INT, scanning NUMERIC, manrewards INT, cmd INT)";
+
+        _command.ExecuteNonQuery();
+
+        // make table for session information
+
+        // trial type numbers
+        _command.CommandText = "create table trialInfo (baseline INT, training INT, test INT)";
+        _command.ExecuteNonQuery();
+
+       
+    }
+
+    void LateUpdate()
+    {
+
+        _command.CommandText = "insert into data (time , trialnum, pos, dz, lick, reward," +
+        "tstart, teleport, rzone, scanning, manrewards, cmd) values (" + Time.realtimeSinceStartup + "," +  numTraversals +
+        "," + transform.position.z + "," + rr.true_delta_z + "," + dl.c_1 + "," + dl.r + "," + pc.tstartFlag + "," + pc.tendFlag + "," +
+        pc.rzoneFlag + "," + ttls.scanning + "," + pc.mRewardFlag + "," + pc.cmd + ")";
+        //Debug.Log(_command.CommandText);
+        _command.ExecuteNonQuery();
+
+        
+
+
+
+    }
+
+    void OnApplicationQuit()
+    {
+        _command.Dispose();
+        _command = null;
+
+        _connection.Close();
+        _connection = null;
+
+        File.Copy(localPrefix + ".sqlite", serverPrefix + ".sqlite", true);
+
+        string sess_connectionString = "Data Source=G:\\My Drive\\VR_Data\\TwoTower\\behavior.sqlite;Version=3;";
+        IDbConnection db_connection;
+        db_connection = (IDbConnection)new SqliteConnection(sess_connectionString);
+        db_connection.Open();
+        IDbCommand db_command = db_connection.CreateCommand();
+        string tmp_date = today.ToString("dd_MM_yyyy");
+        db_command.CommandText = "insert into sessions (MouseName, DateFolder, SessionNumber, Track, RewardCount, Imaging, ImagingRegion, Notes) values ('" + mouse + "', '" + tmp_date + "', "
+            + session + ",'" + sceneName + "', " + numTraversals + ", " + scanning + ",'" + notes.imaging_region + "','" + notes.notes + "')";
+
+        Debug.Log(db_command.CommandText);
+
+        db_command.ExecuteNonQuery();
+
+
+        db_command.Dispose();
+        db_command = null;
+
+        db_connection.Close();
+        db_connection = null;
+
+
+    }
+
+}
