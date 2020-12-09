7ee2ec775c4e215275f5db4ea1e73154373b8c08
markplitt <markplitt@gmail.com>
2020-12-09 14:46:51 -0800

    neuromods functions started

---
+++ Assets/Scripts/NeuroMods/PC_NeuroMods.cs
@@ -0,0 +1,462 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+
+
+
+
+public class PC_NeuroMods : MonoBehaviour
+{
+
+
+    private GameObject player;
+    
+    private GameObject blackCam;
+    private GameObject panoCam;
+    private GameObject reward0;
+    private GameObject reward1;
+    private GameObject reward;
+    
+    
+
+
+    private Rigidbody rb;
+
+    private SP_TwoTower sp;
+    private DL_TwoTower dl;
+    private RR_TwoTower rotary;
+
+    private bool reward_dir;
+
+
+    private Vector3 initialPosition;
+    private Vector3 movement;
+
+    private static bool created = false;
+    private int r;
+    private int r_last = 0;
+    //private bool rflag = true;
+
+    public int cmd = 2;
+    private bool flashFlag = false;
+    private float LastRewardTime;
+    private int prevReward = 0;
+
+    public ArrayList LickHistory;
+    public bool bckgndOn = true;
+
+    public float towerJitter;
+    public float wallJitter;
+    public float bckgndJitter;
+
+    public int mRewardFlag = 0;
+    public int rzoneFlag = 0;
+    public int toutzoneFlag = 0;
+    public int tendFlag = 0;
+    public int tstartFlag = 0;
+    private bool NoSkipTO = true;
+
+
+
+
+    public void Awake()
+    {
+
+
+        GameObject player = GameObject.Find("Player");
+        sp = player.GetComponent<SP_TwoTower>();
+        rotary = player.GetComponent<RR_TwoTower>();
+        dl = player.GetComponent<DL_TwoTower>();
+        Debug.Log(sp.sceneName);
+        if (sp.sceneName == "TwoTower_foraging")
+        {
+           
+            reward = GameObject.Find("Reward");
+        }
+        else
+        {
+            
+            reward0 = GameObject.Find("Reward0");
+            reward1 = GameObject.Find("Reward1");
+        }
+       // Debug.Log(sp.sceneName);
+
+
+
+        panoCam = GameObject.Find("panoCamera");
+        panoCam.transform.eulerAngles = new Vector3(0.0f, -90.0f, 0.0f);
+        initialPosition = new Vector3(0f, 6f, -50.0f);
+
+        
+        //towerJitter = .2f * (UnityEngine.Random.value - .5f);
+        //wallJitter = .2f * (UnityEngine.Random.value - .5f);
+        //bckgndJitter = .2f * (UnityEngine.Random.value - .5f);
+
+        LickHistory = new ArrayList();
+    }
+
+    void Start()
+    {
+        StartCoroutine(FlagCheck());
+        if ((sp.sceneName != "SingleMorph") & (sp.sceneName != "SingleMorph_Reversal"))
+        {
+            Debug.Log("failed");
+            towerJitter = .2f * (UnityEngine.Random.value - .5f);
+            wallJitter = .2f * (UnityEngine.Random.value - .5f);
+            bckgndJitter = .2f * (UnityEngine.Random.value - .5f);
+        }
+        else
+        {
+            towerJitter = 0f;
+            wallJitter = 0f;
+            bckgndJitter = 0f;
+        }
+
+    }
+
+    void Update()
+    {
+
+        // make sure rotation angle is 0
+        transform.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+
+        // end game after appropriate number of trials
+        if ((sp.numTraversals >= sp.numTrialsTotal) | (sp.numRewards >= sp.maxRewards & transform.position.z < 0f))
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        }
+
+        if (dl.r > 0 & dl.rflag < 1) { StartCoroutine(DeliverReward(dl.r)); dl.rflag = 1; }; // deliver appropriate reward
+
+        // manual rewards and punishments
+        mRewardFlag = 0;
+        if (Input.GetKeyDown(KeyCode.Q) | Input.GetMouseButtonDown(0)) // reward left - sweetened condensed milk
+        {
+            mRewardFlag = 1;
+            StartCoroutine(DeliverReward(4));
+            sp.numRewards += 1;
+
+        }
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+
+
+        if (other.tag == "Reward")
+        {
+           // if (rflag)
+           // {
+           StartCoroutine(RewardSequence(transform.position.z));
+            //}
+            
+        }
+        else if (other.tag == "Teleport")
+        {
+            //rflag = true;
+
+            if ((sp.sceneName != "SingleMorph") & (sp.sceneName != "SingleMorph_Reversal"))
+            {
+                towerJitter = .2f * (UnityEngine.Random.value - .5f);
+                wallJitter = .2f * (UnityEngine.Random.value - .5f);
+                bckgndJitter = .2f * (UnityEngine.Random.value - .5f);
+            }
+            //towerJitter = .2f * (UnityEngine.Random.value - .5f);
+            //wallJitter = .2f * (UnityEngine.Random.value - .5f);
+            //bckgndJitter = .2f * (UnityEngine.Random.value - .5f);
+
+
+            sp.numTraversals += 1;
+            tendFlag = 1;
+            transform.position = initialPosition;
+            bckgndOn = true;
+
+            StartCoroutine(InterTrialTimeout());
+
+            LastRewardTime = Time.realtimeSinceStartup; // to avoid issues with teleports
+        }
+        else if (other.tag == "Start")
+        {
+            cmd = 0;
+            tstartFlag = 1;
+            
+            //rflag = true;
+        }
+        else if (other.tag == "Timeout")
+        {
+            //if (sp.morph != .5f)
+            //{
+            StartCoroutine(TimeoutSequence(transform.position.z));
+
+            //}
+        }
+        else if (other.tag == "Delay")
+        {
+            bckgndOn = false;
+        }
+
+    }
+
+
+    IEnumerator FlagCheck() {
+      while (true) {
+        yield return new WaitForEndOfFrame();
+        tendFlag = 0;
+        tstartFlag = 0;
+      }
+      yield return null;
+
+    }
+    IEnumerator InterTrialTimeout()
+    {
+
+        rotary.toutBool = 0f;
+        if (prevReward == 0) // omission
+        {
+            yield return new WaitForSeconds(5f + UnityEngine.Random.value * 5f); // 10f); // 7f);
+
+        }
+        else
+        {
+            yield return new WaitForSeconds(1f + UnityEngine.Random.value*4f);
+        }
+
+        rotary.toutBool = 1f;
+        prevReward = 0;
+        yield return null;
+    }
+
+    void OnApplicationQuit()
+    {
+        panoCam.SetActive(false);
+    }
+
+    IEnumerator RewardSequence(float pos)
+    {   // water reward
+        rzoneFlag = 1;
+       
+        //if (sp.morph == .5f) // guarantee no timeout if morph = .5
+        //{
+        //    StartCoroutine(DeliverReward(5));
+        //}
+
+        //bool counted = true;
+        while ((transform.position.z <= pos + 75) & (transform.position.z > 100)) // & (rflag))  //& (counted)
+        {
+            
+            
+            if ((sp.AutoReward) & (transform.position.z > pos + 50)) // & (counted) )
+            {
+     
+               // if (transform.position.z > pos + 50)
+                //{
+                cmd = 4;
+                //counted = false;
+                //rflag = false;
+                    
+                // yield return new WaitForSeconds(.01f);
+                //cmd = 0;
+                LickHistory.Add(.0f);
+                StartCoroutine(DeliverReward(1));
+                sp.numRewards += 1;
+                yield return new WaitForEndOfFrame();
+                break;
+                   
+                //}
+               // else
+                //{
+                //    cmd = 12;
+                //}
+           // } else
+            //{
+             //   cmd = 12;
+            }
+
+            if ((dl.c_1 > 0))// & (counted))
+            {
+                    //counted = false;
+                    //rflag = false;
+                cmd = 4;
+                sp.numRewards += 1;
+                if (sp.morph + wallJitter + bckgndJitter<=.5) //(sp.morph == 0)
+                {
+
+                    LickHistory.Add(-1f); //.66f);
+                }
+                else if (sp.morph + wallJitter + bckgndJitter>.5) //(sp.morph == 1)
+                {
+
+                    LickHistory.Add(1f); //.33f);
+                }
+                else // morph trials
+                {
+
+                    LickHistory.Add(.0f);
+                }
+                //LickHistory.Add(0f);
+                yield return new WaitForEndOfFrame();
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+           // yield return null;
+        }
+        //yield return new WaitForEndOfFrame();
+        //cmd = 2;
+        //yield return new WaitForSeconds(.1f);
+
+        //yield return null;
+        if (sp.sceneName == "TwoTower_foraging")
+        {
+            
+            reward.SetActive(false);
+        }
+        else
+        {
+           
+            reward0.SetActive(false); reward1.SetActive(false);
+        }
+        
+        
+        
+        
+        //cmd = 2;
+        rzoneFlag = 0;
+        yield return new WaitForEndOfFrame();
+        //yield return new WaitForSeconds(.1f);
+        cmd = 2;
+
+    }
+
+    IEnumerator TimeoutSequence(float pos)
+    {
+        toutzoneFlag = 1;
+        if (sp.morph == .5f)
+        {
+            NoSkipTO = false;
+        }
+        else
+        {
+            NoSkipTO = true;
+        }
+
+        while ((transform.position.z<=pos + 65) )
+        {
+            cmd = 0;
+            if (dl.c_1 > 0)
+            {
+                if (sp.morph == 0)
+                {
+
+                    LickHistory.Add(1f); //.66f);
+                }
+                else if (sp.morph == 1)
+                {
+
+                    LickHistory.Add(-1f); //.33f);
+                }
+                else // morph trials
+                {
+
+                    LickHistory.Add(.0f);
+                }
+
+                toutzoneFlag = 0;
+                tendFlag = 1;
+                transform.position = initialPosition;
+                sp.numTraversals += 1;
+                cmd = 2;
+                bckgndOn = true;
+
+
+                if ((sp.sceneName != "SingleMorph") & (sp.sceneName != "SingleMorph_Reversal"))
+                {
+                    towerJitter = .2f * (UnityEngine.Random.value - .5f);
+                    wallJitter = .2f * (UnityEngine.Random.value - .5f);
+                    bckgndJitter = .2f * (UnityEngine.Random.value - .5f);
+                }
+                
+                
+                // rflag = true;
+                // avoid .5 timeout just end trial
+                if (NoSkipTO)
+                {
+                    rotary.toutBool = 0f;
+                    yield return new WaitForSeconds(5f);
+                  //  if (sp.morph == 0f)
+                  //  {
+                  //      yield return new WaitForSeconds(5f);
+                   // }
+                    // sp.morph = nextMorph;
+                    yield return new WaitForSeconds(5f);
+                    rotary.toutBool = 1f;
+                }
+                else
+                {
+                    rotary.toutBool = 0f;
+                    yield return new WaitForSeconds(1f);
+                    rotary.toutBool = 1f;
+                }
+                NoSkipTO = true;
+
+                break;
+            }
+            else
+            {
+                yield return null;
+
+            }
+        }
+        toutzoneFlag = 0;
+
+
+    }
+
+
+
+    IEnumerator LightsOn()
+    {
+        panoCam.SetActive(true);
+        yield return null;
+    }
+
+
+    IEnumerator DeliverReward(int r)
+    { // deliver
+
+        if (r == 1) // reward left
+        {
+            prevReward = 1;
+            //sp.numRewards += 1;
+            yield return null;
+        }
+        else if (r ==4)
+        {
+            cmd = 4;
+            //sp.numRewards += 1;
+            yield return new WaitForSeconds(0.01f);
+            cmd = 0;
+        }
+        else if (r==5)
+        {
+            prevReward = 1;
+            yield return null;
+        }
+        else
+        {
+            yield return null;
+        };
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/NeuroMods/SP_NeuroMods.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+
+public class SP_NeuroMods : MonoBehaviour
+{
+
+
+    private string mouse;
+
+    public float SkipTrialPcnt = 0.0f;
+
+    public bool AutoReward = false;
+
+
+    public int numRewards = 0;
+    public int numRewards_manual = 0;
+    public int rewardFlag = 0;
+
+    public int numTraversals = 0;
+    public int numTrialsTotal;
+    public int maxRewards = 200;
+
+
+    public float morph = 0;
+    public float rDur = 2;
+
+    // for saving data
+    public string localDirectory_pre = "C:/Users/markp/VR_Data/NeuroMods/";
+    public string serverDirectory_pre = "G:\\My Drive\\InVivoDA\\VR_Data\\";
+    public string localDirectory;
+    public string serverDirectory;
+    public string localPrefix;
+    public string serverPrefix;
+    public string sceneName;
+
+    public int session = 1;
+
+
+    private DateTime today;
+    private GameObject player;
+
+    private TrialOrdering_NeuroMods tott;
+    private RR_NeuroMods rr;
+    private DL_NeuroMods dl;
+    private PC_NeuroMods pc;
+    private SbxTTLs_NeuroMods ttls;
+    private Notes notes;
+
+
+
+    private IDbConnection _connection;
+    private IDbCommand _command;
+
+
+
+    public int scanning = 0;
+
+    public void Awake()
+    {
+       
+        player = GameObject.Find("Player");
+        sceneName = SceneManager.GetActiveScene().name;
+
+
+        
+        rr = player.GetComponent<RR_NeuroMods>();
+        dl = player.GetComponent<DL_NeuroMods>();
+        pc = player.GetComponent<PC_NeuroMods>();
+        ttls = player.GetComponent<SbxTTLs_NeuroMods>();
+        notes = player.GetComponent<Notes>();
+        mouse = notes.mouse;
+
+
+        today = DateTime.Today;
+        Debug.Log(today.ToString("dd_MM_yyyy"));
+
+
+        
+        localDirectory = localDirectory_pre + mouse + '/' + today.ToString("dd_MM_yyy") + '/';
+        serverDirectory = serverDirectory_pre + mouse + '/' + today.ToString("dd_MM_yyy") + '/';
+        if (!Directory.Exists(localDirectory))
+        {
+            Directory.CreateDirectory(localDirectory);
+        }
+        if (!Directory.Exists(serverDirectory))
+        {
+            Directory.CreateDirectory(serverDirectory);
+        }
+
+
+
+
+
+        bool nameFlag = true;
+        while (nameFlag)
+        {
+            localPrefix = localDirectory + "/" + sceneName + "_" + session.ToString();
+            serverPrefix = serverDirectory + "/" + sceneName + "_" + session.ToString();
+            if (File.Exists(localPrefix + ".sqlite"))
+            {
+                session++;
+            } else
+            {
+                nameFlag = false;
+                SqliteConnection.CreateFile(localPrefix + ".sqlite");
+            }
+        }
+
+        string connectionString = "Data Source=" + localPrefix + ".sqlite;Version=3;";
+        _connection = (IDbConnection) new SqliteConnection(connectionString);
+        _connection.Open();
+        _command = _connection.CreateCommand();
+        _command.CommandText = "create table data (time REAL, morph REAL, trialnum INT, pos REAL, dz REAL, lick INT, reward INT," +
+        "tstart INT, teleport INT, rzone INT, toutzone INT, towerJitter REAL," +
+        " wallJitter REAL, bckgndJitter REAL, scanning NUMERIC, manrewards INT, cmd INT)";
+        
+        _command.ExecuteNonQuery();
+
+        // make table for session information
+
+        // trial type numbers
+        _command.CommandText = "create table trialInfo (baseline INT, training INT, test INT)";
+        _command.ExecuteNonQuery();
+
+       // if (sceneName == "TwoTower_4Way")
+        //{
+        //    _command.CommandText = "insert into trialInfo (baseline, training, test) values (" + dbtt_4way.numBaselineTrials + ", " + dbtt_4way.numTrainingTrials + ", " + dbtt_4way.numTestTrials + ")";
+        //}
+        //else if (sceneName == "FreqMorph_Decision")
+       // {
+        //    _command.CommandText = "insert into trialInfo (baseline, training, test) values ( 0 , " + dbfm.numTrainingTrials + ", " + db.numTestTrials + ")";
+        //} else 
+        //{
+         //   if (orderBool)
+           // {
+            //    _command.CommandText = "insert into trialInfo (baseline, training, test) values (" + dbtt.numBaselineTrials + ", " + dbtt.numTrainingTrials + ", " + dbtt.numTestTrials + ")";
+           // }
+            //else
+            //{
+             //   _command.CommandText = "insert into trialInfo (baseline, training, test) values (" + tott.numBaselineTrials + ", " + tott.numTrainingTrials + ", " + tott.numTestTrials + ")";
+           // }
+            
+       // }
+        //_command.ExecuteNonQuery();
+    }
+
+    void LateUpdate() {
+
+      _command.CommandText = "insert into data (time , morph , trialnum, pos, dz, lick, reward," +
+      "tstart, teleport, rzone , toutzone, towerJitter," +
+      " wallJitter , bckgndJitter , scanning, manrewards, cmd) values (" + Time.realtimeSinceStartup + "," + morph + "," + numTraversals +
+      "," + transform.position.z + "," + rr.true_delta_z + "," + dl.c_1 + "," + dl.r + "," + pc.tstartFlag + "," + pc.tendFlag + "," +
+      pc.rzoneFlag + "," + pc.toutzoneFlag + ","  + pc.towerJitter + "," + pc.wallJitter + "," +
+      pc.bckgndJitter + "," + ttls.scanning + "," + pc.mRewardFlag + "," + pc.cmd + ")";
+      //Debug.Log(_command.CommandText);
+      _command.ExecuteNonQuery();
+    
+    if (sceneName=="FrameGrabber")
+        {
+            ScreenCapture.CaptureScreenshot("D://FrameGrabber//morph" + morph + "_wallJit" + pc.wallJitter + "_bckgndJit" + pc.bckgndJitter + "_towerJit" + pc.towerJitter + "_pos" + transform.position.z + ".png");
+        }
+
+     
+
+    }
+
+    void OnApplicationQuit()
+    {
+        _command.Dispose();
+        _command = null;
+
+        _connection.Close();
+        _connection = null;
+
+        File.Copy(localPrefix + ".sqlite", serverPrefix + ".sqlite",true);
+
+        string sess_connectionString = "Data Source=G:\\My Drive\\VR_Data\\TwoTower\\behavior.sqlite;Version=3;";
+        IDbConnection db_connection;
+        db_connection = (IDbConnection) new SqliteConnection(sess_connectionString);
+        db_connection.Open();
+        IDbCommand db_command = db_connection.CreateCommand();
+        string tmp_date = today.ToString("dd_MM_yyyy");
+        db_command.CommandText = "insert into sessions (MouseName, DateFolder, SessionNumber, Track, RewardCount, Imaging, ImagingRegion, Notes) values ('" + mouse +  "', '" + tmp_date + "', "
+            + session + ",'"+ sceneName + "', " + numRewards + ", " + scanning + ",'" + notes.imaging_region + "','" + notes.notes + "')";
+
+        Debug.Log(db_command.CommandText);
+
+        db_command.ExecuteNonQuery();
+
+
+        db_command.Dispose();
+        db_command = null;
+
+        db_connection.Close();
+        db_connection = null;
+
+
+    }
+
+}
