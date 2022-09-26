309d68a64440dbd5573c056d748f65121edb2205
Mari Sosa <msosa32@gmail.com>
2022-09-25 18:10:38 -0700

    add arduino code for both arduinos

---
+++ BScope_dual_arduino/ReadQuadratureEncoder/ReadQuadratureEncoder.ino
@@ -0,0 +1,42 @@
+/* Read Quadrature Encoder
+  * Connect Encoder to Pins encoder0PinA, encoder0PinB, and +5V.
+  *
+  */  
+
+
+ int val; 
+ int encoder0PinA = 10;
+ int encoder0PinB = 11;
+ int encoder0Pos = 0;
+ int encoder0PinALast = LOW;
+ int n = LOW;
+ int incomingByte = 0;   
+
+ void setup() { 
+   pinMode (encoder0PinA,INPUT);
+   pinMode (encoder0PinB,INPUT);
+   Serial.begin (115200);
+   
+ } 
+
+ void loop() { 
+   n = digitalRead(encoder0PinA);
+   if ((encoder0PinALast == LOW) && (n == HIGH)) { // if pinA switches 
+     if (digitalRead(encoder0PinB) == LOW) { // if pinA leads pinB; going backwards
+       encoder0Pos--;
+     } else { // else; going forwards
+       encoder0Pos++;
+     }
+   }
+   
+   if (Serial.available()>0) // if Unity frame occured
+   {
+     //read the incoming byte:
+     incomingByte = Serial.read(); // clear serial line
+     Serial.println(encoder0Pos); // send new delta position
+     encoder0Pos = 0; // reset
+   }
+   
+   encoder0PinALast = n; // reset pin value
+ }
+ 
