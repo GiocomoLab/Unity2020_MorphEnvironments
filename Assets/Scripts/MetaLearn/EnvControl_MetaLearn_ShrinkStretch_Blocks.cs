d533693b4f1a1b04f72b1134a2d4ecac0f32660a
Mari Sosa <msosa32@gmail.com>
2024-04-11 17:49:57 -0700

    debugging shrink/stretch - trying whole maze activation

--- Assets/Scripts/MetaLearn/EnvControl_MetaLearn_ShrinkStretch_Blocks.cs
+++ Assets/Scripts/MetaLearn/EnvControl_MetaLearn_ShrinkStretch_Blocks.cs
@@ -8,15 +8,17 @@ public class EnvControl_MetaLearn_ShrinkStretch_Blocks: MonoBehaviour
 
     private GameObject reward1;
     private GameObject reward2;
-    private GameObject reward3;
+    // private GameObject reward3;
     private GameObject player;
-    private GameObject morphmaze;
-    private GameObject xmaze;
-    private SP_NeuroMods sp;
-    private PC_NeuroMods pc;
+    private GameObject basicmaze;
+    private GameObject shrinkmaze;
+    private GameObject stretchmaze;
+    private SP_MetaLearn sp;
+    private PC_MetaLearn pc;
     private Vector3 initialPosition;
 
     private int switchCount = 0;
+    public int ChangeEnvTrial = 60;
 
     private int numTraversalsLocal = -1;
 
@@ -25,31 +27,33 @@ public class EnvControl_MetaLearn_ShrinkStretch_Blocks: MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Player");
-        sp = player.GetComponent<SP_NeuroMods>();
-        pc = player.GetComponent<PC_NeuroMods>();
+        sp = player.GetComponent<SP_MetaLearn>();
+        pc = player.GetComponent<PC_MetaLearn>();
         
         sp.morph = 0f;
 
-        reward3 = GameObject.Find("Reward_A");
-        reward1 = GameObject.Find("Reward_B");
-        reward2 = GameObject.Find("Reward_C");
+        reward1 = GameObject.Find("Reward_A");
+        reward2 = GameObject.Find("Reward_B");
+        // reward2 = GameObject.Find("Reward_C");
 
-        //morphmaze = GameObject.Find("MorphMaze");
+        basicmaze = GameObject.Find("basicmaze");
+        shrinkmaze = GameObject.Find("shrinkmaze");
+        stretchmaze = GameObject.Find("stretchmaze");
      
 
-        towers1 = GameObject.Find("Tower 1");
-        towers2 = GameObject.Find("Tower 2");
-        towers3 = GameObject.Find("RewardTower1");
-        towers4 = GameObject.Find("RewardTower2");
+        // towers1 = GameObject.Find("Tower 1");
+        // towers2 = GameObject.Find("Tower 2");
+        // towers3 = GameObject.Find("RewardTower1");
+        // towers4 = GameObject.Find("RewardTower2");
 
         //reward.transform.position = reward.transform.position + new Vector3(0f, 0f, sp.mrd
         // initial tower positions: 45, 165, 285, 405
-        towers1.transform.position = new Vector3(0f, 0f, 45f)
-        towers2.transform.position = new Vector3(0f, 0f, 165f)
-        towers3.transform.position = new Vector3(0f, 0f, 285f)
-        towers4.transform.position = new Vector3(0f, 0f, 405f)
+        // towers1.transform.position = new Vector3(0f, 0f, 45f)
+        // towers2.transform.position = new Vector3(0f, 0f, 165f)
+        // towers3.transform.position = new Vector3(0f, 0f, 285f)
+        // towers4.transform.position = new Vector3(0f, 0f, 405f)
 
-        //morphmaze.SetActive(true);
+        basicmaze.SetActive(true);
       
     }
 
@@ -64,22 +68,25 @@ public class EnvControl_MetaLearn_ShrinkStretch_Blocks: MonoBehaviour
             {
                 Debug.Log("Switch");
 
-                if (numTraversalsLocal%30==0)
+                if (numTraversalsLocal%ChangeEnvTrial==0)
                 {
-                    sp.morph = 0f; // Mathf.Abs(sp.morph - 1.0f);
-                   // morphmaze.SetActive(true);
+                   basicmaze.SetActive(false);
+                   shrinkmaze.SetActive(true);
+
                     
                 }
-                else if ( ((numTraversalsLocal-10)==0) | ((numTraversalsLocal-10)%30==0) )
+                else if ( ((numTraversalsLocal-ChangeEnvTrial/2)==0) | ((numTraversalsLocal-ChangeEnvTrial/2)%ChangeEnvTrial==0) )
                 {
-                    sp.morph = 1.0f; // Mathf.Abs(sp.morph - 1.0f);
-                    morphmaze.SetActive(true);
+                    
+                    basicmaze.SetActive(true);
+                    shrinkmaze.SetActive(false);
                 
                 }
-                else if ( ((numTraversalsLocal-20)==0) | ((numTraversalsLocal-20)%30==0) )
+                else if ( ((numTraversalsLocal-ChangeEnvTrial/2)==0) | ((numTraversalsLocal-ChangeEnvTrial/2)%ChangeEnvTrial==0) )
                 {
-                    sp.morph = 0.5f;
-                    morphmaze.SetActive(false);
+
+                    stretchmaze.SetActive(true);
+                    basicmaze.SetActive(false);
             
                 }
                     
@@ -87,54 +94,7 @@ public class EnvControl_MetaLearn_ShrinkStretch_Blocks: MonoBehaviour
                 //Debug.Log(switchCount);
             }
 
-            if (sp.morph==0f)
-            {
-                if (UnityEngine.Random.value < sp.SkipTrialPcnt)
-                {
-                    reward1.SetActive(false);
-
-                }
-                else
-                {
-                    reward1.SetActive(true);
-                }
-                
-                reward2.SetActive(false);
-                reward3.SetActive(false);
-            }
-
-            else if (sp.morph==1.0f)
-            {
-                if (UnityEngine.Random.value < sp.SkipTrialPcnt)
-                {
-                    reward2.SetActive(false);
-
-                }
-                else
-                {
-                    reward2.SetActive(true);
-                }
-                
-                reward1.SetActive(false);
-                reward3.SetActive(false);
-
-            }
-            else
-            {
-
-                if (UnityEngine.Random.value < sp.SkipTrialPcnt)
-                {
-                    reward3.SetActive(false);
-
-                }
-                else
-                {
-                    reward3.SetActive(true);
-                }
-                
-                reward1.SetActive(false);
-                reward2.SetActive(false);
-            }
+            
             
 
 
