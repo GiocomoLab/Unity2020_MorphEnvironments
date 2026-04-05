bc07f0306c15cff144846ebaa2f733272ac3261c
Can Dong <candong.iris@gmail.com>
2026-04-05 13:13:16 -0700

    Updated ip address of scanbox PC

--- Assets/Scripts/2DTrack/SbxTTLs_2DTrack.cs
+++ Assets/Scripts/2DTrack/SbxTTLs_2DTrack.cs
@@ -14,7 +14,7 @@ public class SbxTTLs_2DTrack : MonoBehaviour
 
     // prefs 
 
-    private static string IP = "10.124.53.26";  // define in init
+    private static string IP = "10.124.52.114";  // define in init, old ip 10.124.53.26
     private static int port = 7000;  // define in init
 
     // "connection" things
@@ -45,7 +45,7 @@ public class SbxTTLs_2DTrack : MonoBehaviour
         GameObject player = GameObject.Find("Player");
         sp = player.GetComponent<SP_2DTrack>();
         pc = player.GetComponent<PC_2DTrack>();
-        //notes = player.GetComponent<Notes>();
+        notes = player.GetComponent<Notes>();
         Debug.Log(sp.numTraversals);
 
     }
@@ -139,7 +139,7 @@ public class SbxTTLs_2DTrack : MonoBehaviour
     {
         DateTime today = DateTime.Today;
         // set base directory
-        sendString("D" + "F:/Michelle/m4/" + today.ToString("yyyy_MM_dd") + '/');
+        sendString("D" + "F:/Michelle/" + notes.mouse + "/" + today.ToString("yyyy_MM_dd") + '/');
         yield return new WaitForSeconds(1.5f);
         // set first field/final directory
         sendString("A" + sp.sceneName);
