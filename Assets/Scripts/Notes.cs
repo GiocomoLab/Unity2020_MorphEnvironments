7d1a5f744668946af3e496071374138b9c2638b9
Can Dong <candong.iris@gmail.com>
2026-04-07 14:39:39 -0700

    finish training 4/7

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Note : MonoBehaviour {
+public class Notes : MonoBehaviour {
 
     public string mouse;
     public string notes;
